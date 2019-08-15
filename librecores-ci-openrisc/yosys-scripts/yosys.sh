d9abace8e6c1d2b78d32fcf91208d57fb64859a1
Nancy-Chauhan <nancychn1@gmail.com>
2019-08-15 21:25:40 +0530

    Remove yosys-scripts as script can be implemented as a Jenkins  Pipeline in mor1kx and or1k-marocchino project

--- librecores-ci-openrisc/yosys-scripts/yosys.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -x
-cd /src
-
-fusesoc library add $TOPLEVEL /src
-fusesoc run --target=synth $TOPLEVEL
-/test-scripts/extract-yosys-stats.py  < build/${TOPLEVEL}_${VERSION}/synth-icestorm/yosys.log
